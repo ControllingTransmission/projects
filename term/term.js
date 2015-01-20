01256b643ab5d97189db4905fa517c79eefc2cbe
Steve Dekorte <steve@dekorte.com>
2015-01-20 00:50:40 -0800

    message

--- term/term.js
+++ term/term.js
@@ -13,7 +13,7 @@ var box = blessed.box({
   left: 'left',
   height: '100%',
   width: '100%',
-  content: '',
+  content: 'keys are 1, 2, x, y, f and h. Press r to begin.',
   tags: true,
   border: {
     type: 'bg',
@@ -41,6 +41,15 @@ box.drawStyle = '1';
 box.shouldMoveX = true
 box.shouldMoveY = true
 box.shouldHide = false
+box.shouldullScreen = false
+
+box.fullScreen = function()
+{
+	this.left = 0
+	this.top = 0
+	this.width = screen.width
+	this.height = screen.height
+}
 
 box.move = function()
 {
@@ -63,7 +72,8 @@ box.move = function()
 	}	
 }
 
-setInterval(function () {
+step = function()
+{
 	count ++
 	//var chars = ["/", "_", "\\", "|"]
 
@@ -92,22 +102,32 @@ setInterval(function () {
 		box.style.bg = "#000"	
 		box.ch = " "	
 	}
-
+	
+	if (box.shouldullScreen)
+	{
+		box.fullScreen()
+	}
+	
+	/*
+	box.content = box.content + "" + Math.floor(Math.random()*10)
+	if (box.content.length > 5000)
+	{
+		box.content = ""	
+	}
+	*/
 	screen.render();
-}, 1)
+}
 
 
 screen.key(['1', '2'], function(ch, key) 
 {
   	box.drawStyle = ch
-	//box.content = " " + ch
 });
 
 screen.key(['x'], function(ch, key) 
 {
   	box.shouldMoveX = !box.shouldMoveX
 });
-
 
 screen.key(['y'], function(ch, key) 
 {
@@ -118,6 +138,20 @@ screen.key(['h'], function(ch, key)
 {
   	box.shouldHide = !box.shouldHide
 });
+
+screen.key(['f'], function(ch, key) 
+{
+  	box.shouldullScreen = !box.shouldullScreen
+});
+
+isRunning = false
+
+screen.key(['r'], function(ch, key) 
+{
+	isRunning = true
+	box.content = ""
+	setInterval(function () { step() } , 1)
+})
 
 
 
