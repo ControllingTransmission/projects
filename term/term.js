d406489fda7d6178b1d413587615278257dea8eb
Steve Dekorte <steve@dekorte.com>
2015-01-21 17:26:39 -0800

    such change

--- term/term.js
+++ term/term.js
@@ -1,3 +1,6 @@
+
+// --- setup blessed ---------------------------------------
+
 var blessed = require('blessed')
 var program = blessed.program()
 
@@ -6,8 +9,9 @@ screen.smartCSR = true
 screen.fastCSR = true
 screen.useBCE = true
 screen.autoPadding = false
+screen.alloc()
 
-// ------------------------------------------
+// --- utlity methods ---------------------------------------
 
 String.prototype.repeat = function( num )
 {
@@ -41,7 +45,6 @@ Array.prototype.indexOf = function(elt /*, from*/)
 	return -1;
 }
 	
-
 Array.prototype.remove = function(e)
 {
 	var i = this.indexOf(e);
@@ -52,13 +55,13 @@ Array.prototype.remove = function(e)
 	return this;
 }
 
-function pickAny(array)
+Array.prototype.pickAny = function()
 {
-	var i = Math.floor(Math.random() * array.length) % array.length
-	return array[i]
+	var i = Math.floor(Math.random() * this.length) % this.length
+	return this[i]
 }
 
-// ----------------------------------------------
+// --- Thing class, our custom animation objects clone this ----
 
 var things = []
 
@@ -79,6 +82,31 @@ var Thing = {
 	
 	init: function()
 	{
+		this._removeOnStop = []
+	},
+	
+	setup:function()
+	{
+		
+	},
+	
+	newBox: function()
+	{
+		return blessed.box(
+			{
+				top:10,
+				left:10,
+				width:10,
+				height:10,
+				content: '',
+				tags: false,
+				style: 
+				{
+					fg: "#fff"
+				},
+				ch: ' '
+			}
+		);
 	},
 	
 	_count: 0,
@@ -105,12 +133,12 @@ var Thing = {
 	
 	randColor: function()
 	{
-		return pickAny(["#fff", "#f00", "#0f0", "#ff0", "#00f", "#0ff", "#f0f"])
+		return ["#fff", "#f00", "#0f0", "#ff0", "#00f", "#0ff", "#f0f"].pickAny()
 	},
 	
 	randChar: function()
 	{
-		return pickAny(["O", "+", "|", "-"])
+		return ["O", "+", "|", "-"].pickAny()
 	},
 	
 	randX: function()
@@ -128,16 +156,40 @@ var Thing = {
 	{
 	},
 	
+	_isRunning: false,
+	
 	start: function()
 	{
+		this._isRunning = true
 		things.push(this)
 		return this
 	},
 	
 	stop: function()
 	{
+		this._isRunning = false
 		things.remove(this)
+		
+		for (var i = 0; i < this._removeOnStop.length; i++)
+		{
+			screen.remove(this._removeOnStop[i])
+		}
+		
+		this._removeOnStop = []
+		
 		return this
+	},
+	
+	toggleRunning: function()
+	{
+		if (this._isRunning)
+		{
+			this.stop()
+		}
+		else
+		{
+			this.start()
+		}
 	},
 	
 	attachToKey: function(k)
@@ -147,31 +199,77 @@ var Thing = {
 		{
 		  	self.clone().start()
 		});	
+	},
+	
+	toggleAttachToKey: function(k)
+	{
+		var self = this
+		screen.key([k], function(ch, key) 
+		{
+		  	self.toggleRunning()
+		});	
+	},
+		
+	screenAppend: function(e)
+	{
+		screen.append(e)
+		this._removeOnStop.push(e)
+	},
+	
+	screenRemove: function(e)
+	{
+		screen.remove(e)
+		this._removeOnStop.remove(e)
+	},
+}
+
+// --- boxed ------------------------
+
+var Boxed = Thing.clone()
+Boxed._box = null
+
+Boxed.start = function()
+{
+	Thing.start.apply(this)
+	
+	//if (this._box == null)
+	{
+		this._box = this.newBox()
+		this.screenAppend(this._box)
 	}
+}
+
+Boxed.stop = function()
+{
+	Thing.stop.apply(this)
+	this._box = null
 }
 
 // --- hLine -----------------------
 
-var hLine = Thing.clone()
+HLine = Thing.clone()
+HLine._color = '#fff bg'
 
-hLine.render = function()
+HLine.render = function()
 {
-	this._ttl = screen.height -1
+	this._ttl = screen.height - 1
 	program.setx(0)
 	program.sety(this._count % screen.height)
-	program.write(this.blockString(), this.randColor() + ' bg')
-	/*
-	var color = this.randColor() + ' bg'
-	for (var x = 0; x < screen.width; x ++)
-	{
-		program.setx(x)
-		program.sety(this._count % screen.height)
-		program.write(" ", color)
-	}	
-	*/
+	program.write(this.blockString(), this._color)
 }
 
-hLine.attachToKey("h")
+BlueHLine = HLine.clone()
+BlueHLine._color = '#00f bg'
+BlueHLine.attachToKey("h")
+
+GreenHLine = HLine.clone()
+GreenHLine._color = '#0f0 bg'
+GreenHLine.attachToKey("g")
+
+
+RedHLine = HLine.clone()
+RedHLine._color = '#f00 bg'
+RedHLine.attachToKey("j")
 
 // --- vLine -----------------------
 
@@ -207,77 +305,168 @@ vLine.attachToKey("v")
 // --- tDot ------------------------------
 
 var tDot = Thing.clone()
-
 tDot.render = function()
 {
-	//this._ttl = 15
 	program.setx(this.randX())
 	program.sety(this.randY())
 	program.write(" ", this.randColor() + ' bg')
 }	
-
-
 tDot._ttl = 15
-
 tDot.attachToKey("d")
 
-// --- textBlob ------------------------------
+// --- TextBlob ------------------------------
 
-var textBlob = Thing.clone()
+var Blob = Boxed.clone()
 
-textBlob.init = function()
+Blob.start = function()
 {
-	this._box = blessed.box(
-		{
-			top:10,
-			left:10,
-			width:10,
-			height:10,
-			content: '',
-			tags: false,
-			style: 
-			{
-				fg: "#fff"
-			},
-			ch: 'x'
-		}
-	);
-	
+	Boxed.start.apply(this)
 	this._box.ch = this.randChar()
-
-	screen.append(this._box)
 }
 
-textBlob.randPos = function()
+Blob.randBoxFgColor = function()
 {
-	this._box.top = Math.floor(Math.random()*(screen.width  - 2))
-	this._box.left = Math.floor(Math.random()*(screen.height - 2))
-	this._box.width = Math.floor(Math.random()*(screen.width  - this._box.left))
-	this._box.height = Math.floor(Math.random()*(screen.height - this._box.top))
 	this._box.style.fg = this.randColor()
-	//this._box.ch = this.randChar()
-		
 }
 
-textBlob.render = function()
+Blob.randBoxBgColor = function()
 {
-	//Thing.step.apply(this)
-	this.randPos()
+	this._box.style.bg = this.randColor()
 }
 
-textBlob.stop = function()
+Blob.randBoxX = function()
 {
-	Thing.stop.apply(this)
-	screen.remove(this._box)
-	this._box = null
+	var w = this._box.width
+	this._box.left = Math.floor(Math.random()*(screen.width - 2))
+	this._box.width = w
 }
 
-textBlob._ttl = 20
+Blob.randBoxWidth = function()
+{
+	this._box.width = Math.floor(Math.random()*(screen.width  - this._box.left))
+}
 
-textBlob.attachToKey("t")
+Blob.randBoxY = function()
+{
+	var h = this._box.height
+	this._box.top = Math.floor(Math.random()*(screen.height  - 2))
+	this._box.height = h
+}
+
+Blob.randBoxHeight = function()
+{
+	//this._box.height = Math.floor(Math.random()*(screen.height - this._box.top))
+}
+
+Blob.render = function()
+{
+	if (this._kind == "random")
+	{
+		this.randBoxX()	
+		this.randBoxWidth()	
+		this.randBoxY()	
+		//this.randBoxHeight()	
+	}
+	else if (this._kind == "horizontal")
+	{
+		this.randBoxX()
+	}
+	
+	else if (this._kind == "vertical")
+	{
+		this.randBoxY()
+	}
+
+	if (this._isSolid)
+	{
+		this._box.ch = " "
+		this.randBoxBgColor()
+	}
+	else
+	{
+		this._box.style.bg = "#000"
+		this._box.ch = this.randChar()
+		this.randBoxFgColor()
+	}
+}
 
 
-// ----------------------------------------------
+Blob._ttl = 0
+Blob.toggleAttachToKey("q")
+Blob._kind = "random"
+Blob._isSolid = false
+
+
+screen.key(["e"], function(ch, key) 
+{
+  	Blob._kind = "vertical"
+});	
+
+screen.key(["r"], function(ch, key) 
+{
+  	Blob._kind = "horizontal"
+});
+
+screen.key(["t"], function(ch, key) 
+{
+  	Blob._kind = "random"
+});
+
+screen.key(["w"], function(ch, key) 
+{
+  	Blob._isSolid = !Blob._isSolid
+});
+
+
+// --- BigFlash ----------------------------------------
+
+var BigFlash = Boxed.clone()
+
+BigFlash.start = function()
+{
+	Boxed.start.apply(this)
+	this._box.ch = ' '
+	this._box.top = 0
+	this._box.left = 0
+	this._box.width = screen.width
+	this._box.height = screen.height
+	this._box.style.bg = "#fff"
+}
+
+BigFlash.render = function()
+{
+}
+
+BigFlash._ttl = 10
+BigFlash.attachToKey("u")
+
+RedFlash = BigFlash.clone()
+RedFlash.start = function()
+{
+	BigFlash.start.apply(this)
+	this._box.style.bg = "#f00"
+}
+RedFlash.attachToKey("i")
+
+
+GreenFlash = BigFlash.clone()
+GreenFlash.start = function()
+{
+	BigFlash.start.apply(this)
+	this._box.style.bg = "#0f0"
+}
+GreenFlash.attachToKey("o")
+
+BlueFlash = BigFlash.clone()
+BlueFlash.start = function()
+{
+	BigFlash.start.apply(this)
+	this._box.style.bg = "#00f"
+}
+BlueFlash.attachToKey("p")
+
+
+// --- screen render loop -------------------------------------------
 
 screen._age = 0
 
@@ -286,31 +475,67 @@ screen.step = function()
 	//program.alternateBuffer();
 	program.hideCursor()
 	program.clear()
+  	//program.normalBuffer();
 
 	for (var i = 0; i < things.length; i ++)
 	{
-		var thing = things[i]
-		thing.render()
+		things[i].render()
 	}
 		
 	for (var i = 0; i < things.length; i ++)
 	{
-		var thing = things[i]
-		thing.step()
+		things[i].step()
 	}
-	
+
 	program.setx(0)
 	program.sety(0)
 
 	this._age ++
-	
+		
 	screen.render()
 }
 
-setInterval(function () { screen.step() } , 30)
+setInterval(function () { screen.step() } , 40)
+
+// --- escape key -------------------------------------------
 
 screen.key(['escape', 'C-c'], function(ch, key) 
 {
   return process.exit(0);
 });
+
+
+// --- picture -------------------------------------------
+
+/*
+
+var contrib = require('blessed-contrib')
+
+var pic = contrib.picture(
+    { 
+		file: './a.png', 
+		cols: 30, //screen.width,
+		onReady: ready
+	}
+)
+
+pic.step = function () {}
+
+var picture = null
+function ready() 
+{ 
+	//things.push(pic)
+  	pic.setContent(pic.writer.toString())
+	picture = pic
+}
+
+	if (picture)
+	{
+		program.setx(10)
+		program.sety(10)
+		//program.write(picture.content)
+		//picture.render()
+	}
+*/
+
 
